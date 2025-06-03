1ea2e34b43a167eb2e7189c173fa8d13acd28e17
cher-aim <cheraim.cht@gmail.com>
2025-06-03 19:57:05 +0700

    create performance marketing team data mart tables

---
+++ models/intermediate/performance_marketing/model_mart_interacting_customer_orders.sql
@@ -0,0 +1,24 @@
+{{
+  config(
+    materialized = 'table',
+    schema = 'data_mart'
+    )
+}}
+
+WITH customer_interactions AS (
+	SELECT DISTINCT customer_id
+	FROM {{ ref('model_transaction_campaign_interactions') }} ci 
+)
+SELECT 
+	order_id,
+	ot.customer_id,
+	restaurant_id,
+	driver_id,
+	order_datetime,
+	pickup_datetime,
+	delivery_datetime,
+	order_status,
+	total_amount 
+FROM {{ ref('model_transaction_orders') }} ot 
+JOIN customer_interactions ci 
+ON ot.customer_id = ci.customer_id
