c78df2dd8cb8ef22f9511d02cb7b6c2af59a7fd6
cher-aim <cheraim.cht@gmail.com>
2025-06-05 21:42:46 +0700

    refining code

--- models/data_mart/performance_marketing/model_mart_interacting_customer_orders.sql
+++ models/data_mart/performance_marketing/model_mart_interacting_customer_orders.sql
@@ -5,20 +5,18 @@
     )
 }}
 
-WITH customer_interactions AS (
-	SELECT DISTINCT customer_id
-	FROM {{ ref('model_transaction_campaign_interactions') }} ci 
-)
 SELECT 
-	order_id,
+	ot.order_id,
 	ot.customer_id,
-	restaurant_id,
-	driver_id,
-	order_datetime,
-	pickup_datetime,
-	delivery_datetime,
-	order_status,
-	total_amount 
+	ot.restaurant_id,
+	ot.driver_id,
+	ot.order_datetime,
+	ot.pickup_datetime,
+	ot.delivery_datetime,
+	ot.order_status,
+	ot.total_amount 
 FROM {{ ref('model_transaction_orders') }} ot 
-JOIN customer_interactions ci 
-ON ot.customer_id = ci.customer_id
+WHERE customer_id IN (
+	SELECT DISTINCT customer_id 
+	FROM {{ ref('model_transaction_campaign_interactions') }}
+)
